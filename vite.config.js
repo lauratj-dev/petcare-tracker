33bb7abe61b5f1650a85a388a3694670923ee10e
lauratj-dev <lauratj7796@gmail.com>
2026-05-29 10:04:52 +0200

    feat: add PWA icons

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,51 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'pwa-192x192.png', 'pwa-512x512.png'],
+      manifest: {
+        name: 'PetCare Tracker',
+        short_name: 'PetCare',
+        description: 'Gestiona la salud de tus mascotas',
+        theme_color: '#7B2FBE',
+        background_color: '#f0eeff',
+        display: 'standalone',
+        start_url: '/',
+    icons: [
+  {
+    src: '/pwa-192x192.png',
+    sizes: '192x192',
+    type: 'image/png',
+    purpose: 'any'
+  },
+  {
+    src: '/pwa-192x192.png',
+    sizes: '192x192',
+    type: 'image/png',
+    purpose: 'maskable'
+  },
+  {
+    src: '/pwa-512x512.png',
+    sizes: '512x512',
+    type: 'image/png',
+    purpose: 'any'
+  },
+  {
+    src: '/pwa-512x512.png',
+    sizes: '512x512',
+    type: 'image/png',
+    purpose: 'maskable'
+  }
+]
+      }
+    })
   ],
   resolve: {
     alias: {
