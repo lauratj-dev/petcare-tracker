9a410b4d5c31a904f7b64f28af51f23078a6e8ac
lauratj-dev <lauratj7796@gmail.com>
2026-08-06 13:14:05 +0200

    fix: register about route and update project information

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import PetDetailView from '../views/PetDetailView.vue'
+import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -8,14 +9,19 @@ const router = createRouter({
     {
       path: '/',
       name: 'Home',
-      component: HomeView
+      component: HomeView,
     },
     {
       path: '/pet/:id',
       name: 'PetDetail',
-      component: PetDetailView
-    }
-  ]
+      component: PetDetailView,
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: AboutView,
+    },
+  ],
 })
 
 export default router
